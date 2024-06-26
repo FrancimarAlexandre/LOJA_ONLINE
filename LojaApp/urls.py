98cc4a78dd42c52c182f87b2e73faceea9933edf
FrancimarAlexandre <francimaralexandredt@gmail.com>
2024-06-26 15:54:38 -0300

    iniciando página de compras

--- LojaApp/urls.py
+++ LojaApp/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns =[
     path('',index,name = 'index'),
+    path('produto/<int:id>/',PageCompra,name = "PageCompra"),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # suas outras urls aqui
 
--- LojaApp/views.py
+++ LojaApp/views.py
@@ -8,6 +8,7 @@ def index(request):
         products = response.json()
         for product in products:
             prod = {
+                'id':product['id'],
                 'title': product['title'],
                 'image': product['image'],
                 'price': product['price'],
@@ -26,6 +27,7 @@ def index(request):
             products = response.json()
             for product in products:
                 prod = {
+                    'id':product['id'],
                     'title': product['title'],
                     'image': product['image'],
                     'price': product['price'],
@@ -37,3 +39,24 @@ def index(request):
             print(f"Erro: {response.status_code}")
 
     return render(request, 'index.html', {"produto": produtos})
+
+def PageCompra(request,id):
+    response = requests.get('https://fakestoreapi.com/products')
+    produtos = []
+    if response.status_code == 200:
+        products = response.json()
+        for product in products:
+            if product['id'] == id:
+                prod = {
+                    'title': product['title'],
+                    'image': product['image'],
+                    'price': product['price'],
+                    'category': product['category'],
+                    'description': product['description'],
+                }
+                produtos.append(prod)
+                break
+    else:
+        print(f"Erro: {response.status_code}")
+    
+    return render(request,'compra.html',{"produto":produtos})
